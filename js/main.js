c47372a3847e126f1107bc6b75426bf1871e7cb2
lpohren <ikepohren@gmail.com>
2018-04-18 20:44:24 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ app.controller('myCtrl', function($scope, $http, $location) {
 
     $scope.submitLogin = function() {        
         // change page
-        //window.location.assign('/home.html');
+        window.location.assign('/home.html');
         
         // var url = 'http://localhost:3001/login';
         // $http.get(url)
@@ -26,7 +26,6 @@ app.controller('myCtrl', function($scope, $http, $location) {
             $scope.jenkins = response.data.jenkinsMachine;
         });
 
-        alert(123123123)
     }
 
     $scope.openAddClient = function() {        
